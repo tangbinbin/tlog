705d81886a9f839dcb2adad360f378b212e03536
Binbin Tang (唐宾宾) <tangbinbin@le.com>
2017-07-20 16:40:26 +0800

    fix

--- tlog.go
+++ tlog.go
@@ -4,37 +4,46 @@ import (
 	"bufio"
 	"bytes"
 	"fmt"
+	"github.com/Shopify/sarama"
 	"os"
 	"os/exec"
 	"path"
 	"runtime"
 	"strings"
+	"sync"
 	"time"
 )
 
 type Config struct {
-	Dir      string `toml:"dir"`
-	FileSize int    `toml:"filesize"`
-	FileNum  int    `toml:"filenum"`
-	Debug    bool   `toml:"debug"`
+	FileSize int      `toml:"filesize"`
+	FileNum  int      `toml:"filenum"`
+	Debug    bool     `toml:"debug"`
+	Dir      string   `toml:"dir"`
+	Topic    string   `toml:"topic"`
+	Kafka    []string `toml:"kafka"`
 }
 
 var l *Logger
 
 type Logger struct {
-	dir      string
 	fileSize int
 	fileNum  int
-	debug    bool
-	ch       chan *Atom
 	s        int
+	debug    bool
+	toKafka  bool
+	dir      string
+	topic    string
+	kafka    []string
+	ch       chan *Atom
+	bytePool *sync.Pool
 	f        *os.File
 	w        *bufio.Writer
+	producer sarama.AsyncProducer
 }
 
 type Atom struct {
-	file   string
 	line   int
+	file   string
 	format string
 	args   []interface{}
 }
@@ -43,19 +52,37 @@ type Atom struct {
 func Init(config Config) {
 	l = &Logger{
 		dir:      config.Dir,
+		kafka:    config.Kafka,
+		topic:    config.Topic,
 		fileSize: config.FileSize * 1024 * 1024,
 		fileNum:  config.FileNum,
 		debug:    config.Debug,
+		bytePool: &sync.Pool{New: func() interface{} { return new(bytes.Buffer) }},
 		ch:       make(chan *Atom, 1024),
 	}
 	if l.debug {
 		return
 	}
+	if len(l.kafka) != 0 && l.topic != "" {
+		l.toKafka = true
+	}
+
 	os.MkdirAll(l.dir, 0755)
 	l.f, _ = os.OpenFile(filename(), os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
 	fileInfo, _ := os.Stat(filename())
 	l.s = int(fileInfo.Size())
 	l.w = bufio.NewWriterSize(l.f, 4*1024*1024)
+
+	if l.toKafka {
+		kafkaConfig := sarama.NewConfig()
+		kafkaConfig.Producer.Compression = sarama.CompressionSnappy
+		kafkaConfig.Producer.Return.Errors = false
+		kafkaConfig.Producer.Flush.Messages = 200
+		kafkaConfig.Producer.Flush.Bytes = 100000
+		kafkaConfig.Producer.Flush.Frequency = time.Second
+		kafkaConfig.Producer.RequiredAcks = sarama.NoResponse
+		l.producer, _ = sarama.NewAsyncProducer(l.kafka, kafkaConfig)
+	}
 	go l.flush()
 	go l.start()
 }
@@ -134,58 +161,70 @@ func (l *Logger) start() {
 		if l.w.Buffered() > 1024*1024 {
 			l.w.Flush()
 		}
-		now := time.Now()
-		t := now.Nanosecond() / 1000
-		year, month, day := now.Date()
-		hour, minute, second := now.Clock()
-		l.w.Write([]byte{
-			50,
-			48,
-			49,
-			byte(year%10) + 48,
-			45,
-			byte(month/10) + 48,
-			byte(month%10) + 48,
-			45,
-			byte(day/10) + 48,
-			byte(day%10) + 48,
-			32,
-			byte(hour/10) + 48,
-			byte(hour%10) + 48,
-			58,
-			byte(minute/10) + 48,
-			byte(minute%10) + 48,
-			58,
-			byte(second/10) + 48,
-			byte(second%10) + 48,
-			46,
-			byte((t%1000000)/100000) + 48,
-			byte((t%100000)/10000) + 48,
-			byte((t%10000)/1000) + 48,
-			byte((t%1000)/100) + 48,
-			byte((t%100)/10) + 48,
-			byte(t%10) + 48,
-			32,
-		})
-		l.w.WriteString(a.file)
-		l.s += len(a.file)
-		l.w.Write([]byte{
-			58,
-			byte((a.line%10000)/1000) + 48,
-			byte((a.line%1000)/100) + 48,
-			byte((a.line%100)/10) + 48,
-			byte(a.line%10) + 48,
-			32})
-		n := 0
-		if a.format == "" {
-			n, _ = fmt.Fprint(l.w, a.args...)
-		} else {
-			n, _ = fmt.Fprintf(l.w, a.format, a.args...)
-		}
-		l.w.WriteByte(10)
+		n, b := l.format(a)
+		l.w.Write(b)
 		l.s += n
-		l.s += 34
+		if l.toKafka {
+			l.producer.Input() <- &sarama.ProducerMessage{
+				Topic: l.topic,
+				Value: sarama.StringEncoder(string(b)),
+			}
+		}
 	}
+}
+
+func (l *Logger) format(a *Atom) (int, []byte) {
+	w := l.bytePool.Get().(*bytes.Buffer)
+	defer func() {
+		w.Reset()
+		l.bytePool.Put(w)
+	}()
+	now := time.Now()
+	t := now.Nanosecond() / 1000
+	year, month, day := now.Date()
+	hour, minute, second := now.Clock()
+	w.Write([]byte{
+		50, 48, 49,
+		byte(year%10) + 48,
+		45, //"-"
+		byte(month/10) + 48,
+		byte(month%10) + 48,
+		45,
+		byte(day/10) + 48,
+		byte(day%10) + 48,
+		32, //" "
+		byte(hour/10) + 48,
+		byte(hour%10) + 48,
+		58,
+		byte(minute/10) + 48,
+		byte(minute%10) + 48,
+		58,
+		byte(second/10) + 48,
+		byte(second%10) + 48,
+		46,
+		byte((t%1000000)/100000) + 48,
+		byte((t%100000)/10000) + 48,
+		byte((t%10000)/1000) + 48,
+		byte((t%1000)/100) + 48,
+		byte((t%100)/10) + 48,
+		byte(t%10) + 48,
+		32,
+	})
+	w.WriteString(a.file)
+	w.Write([]byte{
+		58,
+		byte((a.line%10000)/1000) + 48,
+		byte((a.line%1000)/100) + 48,
+		byte((a.line%100)/10) + 48,
+		byte(a.line%10) + 48,
+		32})
+	if a.format == "" {
+		fmt.Fprint(w, a.args...)
+	} else {
+		fmt.Fprintf(w, a.format, a.args...)
+	}
+	w.WriteByte(10)
+	return w.Len(), w.Bytes()
 }
 
 func (l *Logger) rm() {
@@ -205,7 +244,7 @@ func (l *Logger) rm() {
 }
 
 func (l *Logger) flush() {
-	for range time.NewTicker(3 * time.Second).C {
+	for range time.NewTicker(time.Second).C {
 		l.ch <- nil
 	}
 }
